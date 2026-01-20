3832bfd10dedbbc32e86ace7a76124f4d971e0bf
Mohana Dharshini T <cherrykutty8098@gmail.com>
2026-01-20 12:02:00 +0530

    Add files via upload

---
+++ EX 3-Designing Multivariate Patterns/EX-3.R
@@ -0,0 +1,35 @@
+
+install.packages(c("GGally", "dplyr"))
+library(GGally)
+library(dplyr)
+
+health <- read.csv("C:/Users/student/Downloads/3.healthcare_data.csv")
+
+str(health)
+head(health)
+
+health <- health %>%
+  mutate(
+    Age_Group = cut(
+      Age,
+      breaks = c(0, 30, 50, 100),
+      labels = c("Young", "Middle", "Senior")
+    )
+  )
+ggpairs(
+  health,
+  columns = c("Age", "Blood_Pressure", "Cholesterol", "Glucose_Level"),
+  aes(color = Age_Group, alpha = 0.7),
+  title = "Scatter Plot Matrix of Health Indicators"
+)
+
+health_numeric <- health %>%
+  select(Age, Blood_Pressure, Cholesterol, Glucose_Level)
+
+correlation_matrix <- cor(
+  health_numeric,
+  use = "complete.obs"
+)
+
+print(correlation_matrix)
+
